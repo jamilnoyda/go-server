8c66ca935673219b0596e405243e8ea149c1193b
jamil noyda <jamil.noyda@hpe.com>
2023-11-09 10:51:06 +0530

    sub routes testing

---
+++ user_app/user.go
@@ -0,0 +1,29 @@
+import (
+	"fmt"
+	"html"
+	"net/http"
+	"runtime/debug"
+	"strings"
+)
+
+func version(w http.ResponseWriter, r *http.Request) {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		http.Error(w, "no build information available", 500)
+		return
+	}
+
+	fmt.Fprintf(w, "<!DOCTYPE html>\n<pre>\n")
+	fmt.Fprintf(w, "%s\n", html.EscapeString(info.String()))
+
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	name := strings.Trim(r.URL.Path, "/")
+	if name == "" {
+		name = "Gopher"
+	}
+
+	fmt.Fprintf(w, "<!DOCTYPE html>\n")
+	fmt.Fprintf(w, "%s, %s!\n", *greeting, html.EscapeString(name))
+}
